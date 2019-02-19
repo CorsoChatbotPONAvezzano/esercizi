5b930ffe5bdd47afa47163c088f7cb9751ee5180
Gianluca Rossetti <47783773+GianlucaRossi46@users.noreply.github.com>
2019-02-19 15:42:17 +0100

    Update GianlucaRossi46.py.py

--- GianlucaRossi46.py.py
+++ GianlucaRossi46.py.py
@@ -1,29 +1,32 @@
-
 from yaml import load, dump
 dialoghi = {}
 
-dialoghi['ciao'] = 'salve, mi dica'
-dialoghi['buongiorno'] = 'che vuoi'
-dialoghi['che ore sono'] = 'e io che ne so'
-dialoghi['dove è la stazione'] = 'sei troppo piccolo per saperlo'
-dialoghi['perchè?'] = 'non sono affari tuoi'
-dialoghi['cosa pensi di me?'] = 'perchè dovrei risponderti?'
+def Inizializzazione():
+    global dialoghi
+    dialoghi['ciao'] = 'salve, mi dica'
+    dialoghi['buongiorno'] = 'che vuoi'
 
-while True:
-    domanda = input('domanda ?')
-    if domanda == 'fine':
-        break
-    if domanda in dialoghi:
-       risposta = dialoghi[domanda]
-       print(risposta)
-    else:
-        print('questa è una nuovo richiesta')
-        risposta = input('risposta ?')
-        dialoghi[domanda] = risposta
+def salva(nome_file):
+    global dialoghi
+    file = open(f'nome_file).yaml','w',ecoding = 'utf-9')
+    dati = dump(dialoghi, default_flow_style=False)
+    file.write(dati)
+    file.close()
 
-file = open('dialoghi.yaml', 'w', encoding = 'utf-8')
-dati = dump(dialoghi, default_flow_style=False)
-file.write(dati)
-file.close()
+if __name__ == '__main__':
+    Inizializzazione()
+    while True:
+        domanda = input('domanda ?')
+        if domanda == 'fine':
+            break
+        if domanda in dialoghi:
+           risposta = dialoghi[domanda]
+           print(risposta)
+        else:
+             print('questa è una nuovo richiesta')
+             risposta = input('risposta ?')
+             dialoghi[domanda] = risposta
+     salva('dialoghi')
 
 print('finito')
+
