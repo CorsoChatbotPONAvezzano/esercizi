fce5e0e03ffe187150b30d557ed9544af60e2734
Gianluca Rossetti <47783773+GianlucaRossi46@users.noreply.github.com>
2019-02-19 16:28:33 +0100

    Update GianlucaRossi46.py.py

--- GianlucaRossi46.py.py
+++ GianlucaRossi46.py.py
@@ -1,10 +1,14 @@
+
 from yaml import load, dump
 dialoghi = {}
-
-def Inizializzazione():
+def inizializzazione():
     global dialoghi
     dialoghi['ciao'] = 'salve, mi dica'
     dialoghi['buongiorno'] = 'che vuoi'
+    dialoghi['che ore sono'] = 'e io che ne so'
+    dialoghi['dove è la stazione'] = 'sei troppo piccolo per saperlo'
+    dialoghi['perchè?'] = 'non sono affari tuoi'
+    dialoghi['cosa pensi di me?'] = 'perchè dovrei risponderti?'
 
 def salva(nome_file):
     global dialoghi
@@ -15,18 +19,18 @@ def salva(nome_file):
 
 if __name__ == '__main__':
     Inizializzazione()
-    while True:
-        domanda = input('domanda ?')
-        if domanda == 'fine':
-            break
-        if domanda in dialoghi:
-           risposta = dialoghi[domanda]
-           print(risposta)
-        else:
-             print('questa è una nuovo richiesta')
-             risposta = input('risposta ?')
-             dialoghi[domanda] = risposta
-     salva('dialoghi')
+while True:
+    domanda = input('domanda ?')
+    if domanda == 'fine':
+        break
+    if domanda in dialoghi:
+       risposta = dialoghi[domanda]
+       print(risposta)
+    else:
+        print('questa è una nuovo richiesta')
+        risposta = input('risposta ?')
+        dialoghi[domanda] = risposta
+        salva('dialoghi')
 
 print('finito')
 
