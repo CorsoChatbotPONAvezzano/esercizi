2bbc96e7c449b719ea9f7bb55a31db3263dcd1d9
valezeve01 <47784200+valezeve01@users.noreply.github.com>
2019-02-19 16:37:01 +0100

    Add files via upload

---
+++ ValeZeve2.py
@@ -0,0 +1,39 @@
+from yaml import load, dump
+dialoghi = ()
+
+def Inizializzazione():
+    global dialoghi
+    dialoghi('ciao') = 'salve, mi dica'
+    dialoghi('buoongiorno') = 'che vuoi'
+
+def salva(nome_file):
+    global dialoghi
+    file = open(f'(nome_file).ymal' , 'w' , encoding = 'utf-8')
+    dati = dump(dialoghi, default_flow_style=False)
+    file.write(dati)
+    file.close()
+
+def carica(nome_file):
+    global dialoghi
+    file = open (f'(nome_file).yaml', 'r' , encoding = 'out-8')
+    dati = file.read()
+    file.close()
+    dialoghi = load(dati)
+
+if __name__== '__main__':
+    Inizializzazione()
+    carica('dialoghi')
+    while True:
+        domanda = input('domanda ?')
+        if domanda == 'fine':
+            break
+        if domanda in dialoghi:
+            risposta = dialoghi(domanda)
+            print(risposta)
+        else:
+            print('questa è una nuova richiesta')
+            risposta = input('risposta ?')
+            dialoghi(domanda) = risposta
+        salva('dialoghi')
+
+    print('finito')
