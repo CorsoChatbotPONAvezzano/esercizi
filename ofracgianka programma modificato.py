53fe968235af48446822705502821e2de08f3a57
fracgianka <46492380+fracgianka@users.noreply.github.com>
2019-02-19 16:21:16 +0100

    Add files via upload

---
+++ ofracgianka programma modificato.py
@@ -0,0 +1,38 @@
+
+from yaml import load, dump
+dialoghi = {}
+def Inizializzazione() :
+    dialoghi['ciao'] = 'salve, mi dica'
+    dialoghi['buongiorno'] = 'che vuoi'
+    dialoghi['che ore sono'] = 'e io che ne so'
+    dialoghi['dove è la stazione'] = 'sei troppo piccolo per saperlo'
+    dialoghi['perchè?'] = 'non sono affari tuoi'
+    dialoghi['cosa pensi di me?'] = 'perchè dovrei risponderti?'
+
+def salva(nome_file) :
+    global dialoghi
+    file = open(f'dialoghi.yaml', 'w', encoding = 'utf-8')
+    dati=dump(dialoghi, default_flow_style=False)
+    file.write(dati)
+    file.close()
+
+if __name__=='__main__':
+   Inizializzazione()                   
+   while True:
+    domanda = input('domanda ?')
+    if domanda == 'fine':
+        break
+    if domanda in dialoghi:
+       risposta = dialoghi[domanda]
+       print(risposta)
+    else:
+        print('questa è una nuovo richiesta')
+        risposta = input('risposta ?')
+        dialoghi[domanda] = risposta
+
+file = open('dialoghi.yaml', 'w', encoding = 'utf-8')
+dati = dump(dialoghi, default_flow_style=False)
+file.write(dati)
+file.close()
+
+print('finito')
